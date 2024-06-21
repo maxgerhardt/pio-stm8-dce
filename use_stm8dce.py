4dc4b13a4a20e8172aa4c258a5767dacec6195ae
dzantemir <ivan23654623@yahoo.com>
2024-06-21 10:37:41 +0300

    Update use_stm8dce.py
    долго боролся с платформио с его кривой компиляцией короче добавил опцию stm8dce_flags её надо добавить в platformio.ini 
    stm8dce_flags = -v
    
    она передаст флаги в stm8dce

--- use_stm8dce.py
+++ use_stm8dce.py
@@ -7,32 +7,25 @@ Import("env")
 if shutil.which("stm8dce") is None:
     sys.stderr.write("stm8dce tool not found, install it via https://github.com/CTXz/STM8-DCE")
     env.Exit(-1)
-
-env.Append(CFLAGS=["-S"])#тут короче хрень, платформио убирает этот флаг при компиляции то есть гъде-то ставит гъде-то нет короч подремонтируйте
-
+    
 def optimize_asm(source, target, env):
-
-    all_path = ""
-
+    
     temp_out_dir = os.path.join(env.subst("$BUILD_DIR"), "optim_output")
     if not os.path.isdir(temp_out_dir):
         os.mkdir(temp_out_dir)
 
-
-    for x in source:
-        tmp_path=os.path.splitext(str(x))[0]+".asm" 
-        all_path+= " " + tmp_path
-        if not os.path.isfile(tmp_path): 
-            os.rename(str(x),tmp_path)
+    env.Execute("stm8dce "  + 
+                env.GetProjectOption("stm8dce_flags", default="")  +   
+                " -o " + 
+                temp_out_dir +
+                  " "+
+                  " ".join( [os.path.splitext(str(x))[0]+".asm"   for x in source]))  
     
-    env.Execute("stm8dce -o "+ temp_out_dir + all_path)    
-  
     for x in source:
         tmp_path=os.path.join(temp_out_dir,os.path.splitext(os.path.basename(str(x)))[0]+".asm")
         env.Execute("$AS -plosg -ff -o " + str(x) + " " +tmp_path )
         os.remove(tmp_path)
 
     os.rmdir(temp_out_dir)
-
 
 env.AddPreAction("$BUILD_DIR/${PROGNAME}.elf", optimize_asm)
