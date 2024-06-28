a168fc2acb84cda3f279320ec2697a68cf87969d
Дзантемир Дзгоев <ivan23654623@yahoo.com>
2024-06-28 16:14:20 +0300

    Update use_stm8dce.py

--- use_stm8dce.py
+++ use_stm8dce.py
@@ -18,9 +18,7 @@ if missing:
     except:
          sys.stderr.write("stm8dce tool not found, install it via https://github.com/CTXz/STM8-DCE")
          env.Exit(-1)
-
-
-
+        
 def optimize_asm(source, target, env):
     
     temp_out_dir = os.path.join(env.subst("$BUILD_DIR"), "optim_output")
@@ -29,12 +27,12 @@ def optimize_asm(source, target, env):
         os.mkdir(temp_out_dir)
 
     asm_path=""
-
+    
     for x in source:
       tst_asm_pth= os.path.splitext(str(x))[0]+".asm"  
       if  os.path.isfile(tst_asm_pth):
         asm_path += ' "' + tst_asm_pth +'"'
-  
+          
     env.Execute(env.VerboseAction("stm8dce "  + 
     " -o "  + 
     '"' + temp_out_dir + 
@@ -43,12 +41,10 @@ def optimize_asm(source, target, env):
     (" -v" if int(ARGUMENTS.get("PIOVERBOSE", 0)) else "") +
     ' "' +
     '" "'.join(map(str,source))+ '"' + asm_path, "STM8DCE CODE OPTIMIZATION"))
-  
     
     for x in source:
         tmp_path= os.path.join(temp_out_dir,os.path.splitext(os.path.basename(str(x)))[0]+".asm") 
         if  os.path.isfile(tmp_path):
             env.Execute(env.VerboseAction("$AS -plosg -ff -o " + '"' +str(x) +'" "' +tmp_path+'"',"COMPILING "+str(x) ))
-
-
+            
 env.AddPreAction("$BUILD_DIR/${PROGNAME}.elf", env.VerboseAction(optimize_asm,"EXTRA SCRIPT"))
