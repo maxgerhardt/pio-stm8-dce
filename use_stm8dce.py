6980dd8629400dce371b67d968bc0e8cc1a1c637
Дзантемир Дзгоев <ivan23654623@yahoo.com>
2024-06-29 08:11:13 +0300

    Update use_stm8dce.py

--- use_stm8dce.py
+++ use_stm8dce.py
@@ -29,9 +29,9 @@ def optimize_asm(source, target, env):
     asm_path=""
     
     for x in source:
-      tst_asm_pth= os.path.splitext(str(x))[0]+".asm"  
-      if  os.path.isfile(tst_asm_pth):
-        asm_path +=' "'+tst_asm_pth +'"'
+        tst_asm_pth= os.path.splitext(str(x))[0]+".asm"  
+        if  os.path.isfile(tst_asm_pth):
+            asm_path +=' "'+tst_asm_pth +'"'
           
     env.Execute(env.VerboseAction("stm8dce "  + 
     " -o "  + 
